bedbfcf8f242ca9b2683a0eee3f116702dcc6647
sschellhoff <soeren.schellhoff@gmail.com>
2015-08-18 16:49:04 +0200

    first project setup, +tokentype enum, +token class, +scanner class

---
+++ slll/s3lc/Token.cpp
@@ -0,0 +1,28 @@
+#include "Token.h"
+
+using namespace slll;
+
+Token::Token(TokenType type, std::string value):type(type), value(value)
+{
+}
+
+
+Token::~Token()
+{
+}
+
+TokenType Token::Type() const
+{
+	return type;
+}
+
+const std::string & Token::Value() const
+{
+	return value;
+}
+
+std::ostream & slll::operator<<(std::ostream & stream, const Token & token)
+{
+	stream << "(" << token.Type() << ", " << token.Value() << ")";
+	return stream;
+}
